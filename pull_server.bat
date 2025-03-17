3a367c5a435c5e4b96219142888b6bd91df00814
FroksonTy <52175835+FroksonTy@users.noreply.github.com>
2025-03-17 19:38:39 +0700

    Добавил дополнительную проверку перед стартом скрипта
    Теперь скрипт будет уточнять, действительно ли пользователь хочет выполнить pull

--- pull_server.bat
+++ pull_server.bat
@@ -5,8 +5,17 @@ setlocal
 set GIT_REPO="https://github.com/FroksonTy/Valheim.git"
 set GIT_CLONE_PARENT="E:\git"
 set SERVER_FOLDER="%USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds_local"
- 
+
+:: Запрос подтверждения
+:confirm
+set "choice="
+set /p choice="Are you sure you want to pull world from GitHub? [Y/N]: "
+if /i "%choice%"=="Y" goto git_pull
+if /i "%choice%"=="N" goto exit
+goto confirm
+
 :: Удаляем старую временную папку (если есть)
+:git_pull
 rmdir /s /q %GIT_CLONE_PARENT%\valheim-server 2>nul
 
 :: Клонируем репозиторий во временную папку
@@ -20,3 +29,4 @@ rmdir /s /q %GIT_CLONE_PARENT%\valheim-server 2>nul
 
 echo World files successfully updated from Git!
 pause
+:exit
